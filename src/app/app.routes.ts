ae423692fc00aa9d1ba02dcc2e26fddffdd61895
Mishell C. <jose.catota@snappcorp.com>
2026-01-15 02:34:15 -0500

    Corrijo error de renderización para editar id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ClienteList } from './features/clientes/components/cliente-list/cliente-list';
 import { ClienteForm } from './features/clientes/components/cliente-form/cliente-form';
+import { CuentaList } from './features/cuentas/components/cuenta-list/cuenta-list';
+import { MovimientoList } from './features/movimientos/components/movimiento-list/movimiento-list';
 
 export const routes: Routes = [
     //{ path: 'clientes', component: ClienteList },
@@ -9,4 +11,6 @@ export const routes: Routes = [
     { path: 'clientes/nuevo', component: ClienteForm },
     { path: 'clientes/editar/:id', component: ClienteForm },
     //{ path: 'clientes/:id', component: ClienteDetailComponent },
+    { path: 'cuentas', component: CuentaList },
+    { path: 'movimientos', component: MovimientoList },
 ];
